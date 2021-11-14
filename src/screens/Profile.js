3bed3427f965295e29db09c36a62ad2c48d6f704
jwbak <jwbak2@gmail.com>
2021-11-14 16:47:21 +0900

    [Add] 프로필 UI 추가

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,20 +1,45 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Box, ScrollView, Text } from "native-base";
-
-export const Profile = () => {
-  return (
-    <>
-      <Box></Box>
-    </>
-  );
-};
+import { View, StyleSheet, ScrollView } from "react-native";
+import Friends from "../components/Friends";
+import {
+  Box,
+  Text,
+  Pressable,
+  v3CompatibleTheme,
+  Avatar,
+  HStack,
+  VStack,
+  Spacer,
+  Center,
+} from "native-base";
 
 export default function Profile() {
   return (
-    <Box>
-      <Profile />
-      <ScrollView></ScrollView>
+    <Box style={styles.container}>
+      <Box style={styles.profile}>
+        <Box style={styles.profileInfo}>
+          <Box pl="4" pr="5" py="2">
+            <HStack alignItems="center" space={3}>
+              <Avatar size="xl" bg="violet.300" />
+              <VStack>
+                <Text fontSize="2xl" color="coolGray.800" bold>
+                  사용자 이름
+                </Text>
+                <Text color="coolGray.600">자기소개</Text>
+              </VStack>
+              {/* <Spacer /> */}
+            </HStack>
+          </Box>
+        </Box>
+        {/* <Center style={styles.modifyProfileBtn}> */}
+        <Pressable bg="coolGray.500" style={styles.modifyProfileBtn}>
+          <Text bold color="white">
+            회원정보 수정
+          </Text>
+        </Pressable>
+        {/* </Center> */}
+      </Box>
+      <Friends />
     </Box>
   );
 }
@@ -23,7 +48,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#eeeeee",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  profile: {
+    flex: 0.4,
+    borderColor: "#cccccc",
+    borderBottomWidth: 2,
+    // backgroundColor: "#00ffff",
+  },
+  friendList: {
+    flex: 0.6,
+    // backgroundColor: "#ffff00",
+  },
+  profileInfo: {
+    flex: 4,
+    // backgroundColor: "#ff0000",
+  },
+  modifyProfileBtn: {
+    flex: 1,
     alignItems: "center",
+    marginVertical: 10,
+    marginHorizontal: 50,
+    borderRadius: 15,
+    // width: "50%",
     justifyContent: "center",
   },
 });
