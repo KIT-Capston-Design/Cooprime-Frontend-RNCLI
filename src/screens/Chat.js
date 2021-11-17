297324a1e1e84eb7eeaaba2af272957b166c8685
jinoo7099 <vxc7099@gmail.com>
2021-11-17 20:37:50 +0900

    fix : chat function

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,9 +1,4 @@
-import React, {
-  useState,
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-} from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import database from "@react-native-firebase/database";
 import {
@@ -12,43 +7,62 @@ import {
   renderComposer,
   renderSend,
 } from "../components/InputToolbar";
+import { Alert } from "react-native";
+import messaging from "@react-native-firebase/messaging";
 
+/*
+신고기능
+
+알림기능
+*/
 export default function Chat() {
   const [text, setText] = useState("");
   const [messages, setMessages] = useState([]);
   const [userInfo, setUserInfo] = useState({});
   const [chatRoomName, setChatRoomName] = useState("");
 
-  const renderMessage = (messages = []) =>
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages)
-    );
-
   useEffect(() => {
     setMessages([]);
-    const initMassages = [];
-    // database()
-    //   .ref("/chat_room")
-    //   .once("value")
-    //   .then((snapshot) => {
-    //     snapshot.forEach((item) => {
-    //       const itemVal = item.val();
-    //       const { createdAt, text, user } = item.val();
-    //       const { key: _id } = item;
-    //       const message = { _id, createdAt, text, user };
-    //       initMassages.push(message);
-    //     });
-    //     renderMessage(initMassages);
-    //   });
 
-    const onChildAdd = database()
+    // 디비에서 메세지 다불러오기
+    const initMassages = [];
+    database()
+      .ref("/chat_room") // /chat_room/ + chatRoomName + / 으로 변경
+      .orderByChild("createdAt")
+      .once("value")
+      .then((snapshot) => {
+        snapshot.forEach((item) => {
+          const message = parse(item);
+          initMassages.push(message);
+        });
+        initMassages.reverse();
+        return initMassages;
+      })
+      .then((msg) => {
+        renderMessage(msg);
+      });
+
+    // 새로운 메세지 출력
+    database()
       .ref("/chat_room")
+      .orderByChild("createdAt")
+      .startAt(Date.now())
       .on("child_added", (snapshot) => {
         console.log(snapshot);
         renderMessage(parse(snapshot));
       });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+    return unsubscribe;
   }, []);
 
+  // snapshot to message json form
   const parse = (snapshot) => {
     const { createdAt, text, user } = snapshot.val();
     const { key: _id } = snapshot;
@@ -56,6 +70,7 @@ export default function Chat() {
     return message;
   };
 
+  // 메세지 보내기
   const onSend = useCallback((messages = []) => {
     const reference = database().ref("/chat_room");
     const { text, user } = messages[0];
@@ -66,6 +81,12 @@ export default function Chat() {
     };
     reference.push(message).then(() => console.log("Data set."));
   }, []);
+
+  // 메세지 화면에 render
+  const renderMessage = (messages = []) =>
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
 
   return (
     <GiftedChat
